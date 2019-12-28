c382067c147351071689a007007e562dc416241b
Simen Holmestad <simehol@stud.ntnu.no>
2019-12-28 21:31:55 +0100

    Add logic for doing a move
    Logic for checking if the game is won is not yet implemented, and thus
    the tests are commented out.

---
+++ __tests__/moveTests.js
@@ -0,0 +1,416 @@
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+import app from './../app';
+import Game from './../app/models/game';
+import initialGameData from './../app/constants/initialGameData';
+
+const request = supertest(app);
+
+function createNewGame() {
+  // This is needed so the initialGameData does not change.
+  return new Game(JSON.parse(JSON.stringify(initialGameData)));
+};
+
+describe('move tests', () => {
+  beforeAll(async () => {
+    // Connect to database
+    const url = `mongodb://127.0.0.1/move-test`;
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Delete every game in database
+    await Game.deleteMany();
+  });
+
+  afterAll(async () => {
+    // Remove the database
+    mongoose.connection.db.dropDatabase();
+    // Close the connection to MongoDB
+    await mongoose.connection.close();
+  });
+
+  afterEach(async () => {
+    await Game.deleteMany();
+  });
+
+  it('test legal first moves', async done => {
+    const game = createNewGame();
+    game.name = "My first game";
+    await game.save();
+
+    const response1 = await request.post('/api/games/' + game._id + '/move')
+      	  .send({
+            xPos: 0,
+            yPos: 0,
+            player: "O"
+          });
+
+    expect(response1.status).toBe(200);
+    expect(response1.body.name).toEqual('My first game');
+    expect(response1.body._id).not.toEqual(undefined);
+    expect(response1.body.error).toEqual(undefined);
+
+    expect(response1.body.boardState[0][0]).toEqual("O");
+    expect(response1.body.nextToMove).toEqual("X");
+    expect(response1.body.history).toEqual([[0, 0, "O"]]);
+
+    const response2 = await request.post('/api/games/' + game._id + '/move')
+      	  .send({
+            xPos: 1,
+            yPos: 0,
+            player: "X"
+          });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body.name).toEqual('My first game');
+    expect(response2.body._id).not.toEqual(undefined);
+    expect(response2.body.error).toEqual(undefined);
+
+    expect(response2.body.boardState[0][0]).toEqual("O");
+    expect(response2.body.boardState[0][1]).toEqual("X");
+    expect(response2.body.nextToMove).toEqual("O");
+    expect(response2.body.history).toEqual([[0, 0, "O"], [0, 1, "X"]]);
+
+    done();
+
+  });
+
+  it('test moves outside boards', async done => {
+    const game = createNewGame();
+    await game.save();
+
+    let response = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: -1,
+          yPos: 0,
+          player: "O"
+        });
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toEqual(undefined);
+    expect(response.body.error).toEqual("Position was outside of board. Only values between 0 and 20 allowed. Received x:-1 and y:0");
+
+    response = await request.post('/api/games/' + game._id + '/move')
+      .send({
+        xPos: 0,
+        yPos: -1,
+        player: "O"
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toEqual(undefined);
+    expect(response.body.error).toEqual("Position was outside of board. Only values between 0 and 20 allowed. Received x:0 and y:-1");
+
+    response = await request.post('/api/games/' + game._id + '/move')
+      .send({
+        xPos: 22,
+        yPos: 0,
+        player: "O"
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toEqual(undefined);
+    expect(response.body.error).toEqual("Position was outside of board. Only values between 0 and 20 allowed. Received x:22 and y:0");
+
+    response = await request.post('/api/games/' + game._id + '/move')
+      .send({
+        xPos: 0,
+        yPos: 21,
+        player: "O"
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toEqual(undefined);
+    expect(response.body.error).toEqual("Position was outside of board. Only values between 0 and 20 allowed. Received x:0 and y:21");
+
+    done();
+  });
+
+  it('test moving when not your turn', async done => {
+    const game = createNewGame();
+    await game.save();
+
+    let response1 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "X"
+        });
+
+    expect(response1.status).toBe(200);
+    expect(response1.body._id).toEqual(undefined);
+    expect(response1.body.error).toEqual("It is not X's turn.");
+
+    await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "O"
+        });
+
+    let response2 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 1,
+          yPos: 0,
+          player: "O"
+        });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body._id).toEqual(undefined);
+    expect(response2.body.error).toEqual("It is not O's turn.");
+
+    done();
+  });
+
+  it('test moves on filled tiles', async done => {
+    const game = createNewGame();
+    game.boardState[0][0] = "O";
+    game.boardState[16][15] = "X";
+    await game.save();
+
+    let response1 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "O"
+        });
+
+    expect(response1.status).toBe(200);
+    expect(response1.body._id).toEqual(undefined);
+    expect(response1.body.error).toEqual("There is already a cross or circle at tile x:0, y:0");
+
+    let response2 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 15,
+          yPos: 16,
+          player: "O"
+        });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body._id).toEqual(undefined);
+    expect(response2.body.error).toEqual("There is already a cross or circle at tile x:15, y:16");
+
+    done();
+  });
+
+  it('test moving on nonexistent board', async done => {
+    const fake_game_id = "5dfcf3c43f0ed01259baaaf5";
+        let response = await request.post('/api/games/' + fake_game_id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "O"
+        });
+    expect(response.status).toBe(404);
+    done();
+  });
+
+  it('test moving on finished board', async done => {
+    const game = createNewGame();
+    game.winner = "X";
+    await game.save();
+
+    let response = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "O"
+        });
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toEqual(undefined);
+    expect(response.body.error).toEqual("This game is finished. No more moves are allowed");
+    done();
+  });
+
+  it('test missing post data', async done => {
+    const game = createNewGame();
+    await game.save();
+
+    let response1 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          yPos: 0,
+          player: "O"
+        });
+
+    expect(response1.status).toBe(200);
+    expect(response1.body._id).toEqual(undefined);
+    expect(response1.body.error).toEqual("Missing argument: xPos");
+
+    let response2 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          yPos: 0,
+        });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body._id).toEqual(undefined);
+    expect(response2.body.error).toEqual("Missing argument: xPos, player");
+
+    let response3 = await request.post('/api/games/' + game._id + '/move');
+
+    expect(response3.status).toBe(200);
+    expect(response3.body._id).toEqual(undefined);
+    expect(response3.body.error).toEqual("Missing argument: xPos, yPos, player");
+    done();
+  });
+
+  it('test illegal player', async done => {
+    const game = createNewGame();
+    await game.save();
+
+    let response1 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "Freddy"
+        });
+
+    expect(response1.status).toBe(200);
+    expect(response1.body._id).toEqual(undefined);
+    expect(response1.body.error).toEqual('Illegal value for player. Must be "O" or "X"');
+
+    let response2 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "Player1"
+        });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body._id).toEqual(undefined);
+    expect(response2.body.error).toEqual('Illegal value for player. Must be "O" or "X"');
+
+    done();
+  });
+
+  it('test illegal coordinates', async done => {
+    const game = createNewGame();
+    await game.save();
+
+    let response1 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: "0",
+          yPos: "0",
+          player: "O"
+        });
+
+    expect(response1.status).toBe(200);
+    expect(response1.body._id).toEqual(undefined);
+    expect(response1.body.error).toEqual('xPos and yPos must be integers');
+
+    let response2 = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: "Den første",
+          player: "O"
+        });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body._id).toEqual(undefined);
+    expect(response2.body.error).toEqual('xPos and yPos must be integers');
+
+    done();
+  });
+
+  it('test last move made', async done => {
+    const game = createNewGame();
+    await game.save();
+
+    let response = await request.post('/api/games/' + game._id + '/move')
+      	.send({
+          xPos: 0,
+          yPos: 0,
+          player: "O"
+        });
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).not.toEqual(undefined);
+    expect(response.body.timeStarted === response.body.lastMoveMade).toEqual(false);
+
+    done();
+  });
+
+  // it('test winning horizontally', async done => {
+  //   const game = createNewGame();
+  //   game.board[0][0] = "O";
+  //   game.board[0][1] = "O";
+  //   game.board[0][2] = "O";
+  //   game.board[0][3] = "O";
+  //   await game.save();
+
+  //   let response = await request.post('/api/games/' + game._id + '/move')
+  //     	.send({
+  //         xPos: 4,
+  //         yPos: 0,
+  //         player: "O"
+  //       });
+
+  //   expect(response.body.winner).toBe("O");
+  //   expect(response.body.nextToMove).toBe(null);
+  //   expect(response.body.winningLine).toBe( [ [0,0], [0,1], [0,2], [0,3], [0,4] ] );
+
+  //   done();
+  // });
+
+  // it('test winning vertically', async done => {
+  //   const game = createNewGame();
+  //   game.board[0][0] = "X";
+  //   game.board[1][0] = "X";
+  //   game.board[2][0] = "X";
+  //   game.board[3][0] = "X";
+  //   await game.save();
+
+  //   let response = await request.post('/api/games/' + game._id + '/move')
+  //     	.send({
+  //         xPos: 0,
+  //         yPos: 4,
+  //         player: "X"
+  //       });
+
+  //   expect(response.body.winner).toBe("X");
+  //   expect(response.body.nextToMove).toBe(null);
+  //   expect(response.body.winningLine).toBe( [ [0,0], [1,0], [2,0], [3,0], [4,0] ] );
+
+  //   done();
+  // });
+
+  // it('test winning diagonally', async done => {
+  //   const game = createNewGame();
+  //   game.board[0][0] = "X";
+  //   game.board[1][1] = "X";
+  //   game.board[2][2] = "X";
+  //   game.board[3][3] = "X";
+  //   await game.save();
+
+  //   let response = await request.post('/api/games/' + game._id + '/move')
+  //     	.send({
+  //         xPos: 4,
+  //         yPos: 4,
+  //         player: "X"
+  //       });
+
+  //   expect(response.body.winner).toBe("X");
+  //   expect(response.body.nextToMove).toBe(null);
+  //   expect(response.body.winningLine).toBe( [ [0,0], [1,1], [2,2], [3,3], [4,4] ] );
+
+
+  //   const game2 = new Game(initialGameData);
+  //   game2.board[0][4] = "X";
+  //   game2.board[1][3] = "X";
+  //   game2.board[2][2] = "X";
+  //   game2.board[3][1] = "X";
+  //   await game2.save();
+
+  //   let response2 = await request.post('/api/games/' + game2._id + '/move')
+  //     	.send({
+  //         xPos: 0,
+  //         yPos: 4,
+  //         player: "X"
+  //       });
+
+  //   expect(response2.body.winner).toBe("X");
+  //   expect(response2.body.nextToMove).toBe(null);
+  //   expect(response2.body.winningLine).toBe( [ [0,4], [1,3], [2,2], [3,1], [4,0] ] );
+
+  //   done();
+  // });
+});
--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -51,4 +51,134 @@ router.route('/games/:game_id').get(async (req, res) => {
   }
 });
 
+router.route('/games/:game_id').get(async (req, res) => {
+  try {
+    const gameExists = await Game.exists({ _id: req.params.game_id });
+    if (! gameExists) {
+      res.status(404).send('The game was not found');
+      return;
+    }
+
+    const game = await Game.findById(req.params.game_id);
+    res.json(game);
+  }
+  catch (err) {
+    res.send(err);
+  }
+});
+
+router.route('/games/:game_id/move').post(async (req, res) => {
+  // Check if game in database
+  try {
+    const gameExists = await Game.exists({ _id: req.params.game_id });
+    if (! gameExists) {
+      res.status(404).send('The game was not found');
+      return;
+    }
+  }
+  catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+
+  // Check if the correct post data fields are sent
+  const missingArguments = [];
+  if (req.body.xPos == undefined) {
+    missingArguments.push('xPos');
+  }
+  if (req.body.yPos == undefined) {
+    missingArguments.push('yPos');
+  }
+  if (req.body.player == undefined) {
+    missingArguments.push('player');
+  }
+  if (missingArguments.length != 0) {
+    const errorString = "Missing argument: " + missingArguments.join(", ");
+    res.json({
+      error: errorString
+    });
+    return;
+  }
+
+  const xPos = req.body.xPos;
+  const yPos = req.body.yPos;
+  const player = req.body.player;
+
+  // Find the next player and check if player string is legal
+  let nextPlayer;
+  if (player == "X") {
+    nextPlayer = "O";
+  } else if (player == "O") {
+    nextPlayer = "X";
+  } else {
+    res.json({
+      error: 'Illegal value for player. Must be "O" or "X"'
+    });
+    return;
+  }
+
+  // Check if position values are legal
+  if (!Number.isInteger(xPos) || !Number.isInteger(yPos)) {
+    res.json({
+      error : 'xPos and yPos must be integers'
+    });
+    return;
+  }
+
+  // Check if the positions are inside the board
+  if (xPos < 0 || xPos > 20 || yPos < 0 || yPos > 20) {
+    const errorString = "Position was outside of board. Only values between 0 and 20 allowed. Received x:" + xPos + " and y:" + yPos;
+    res.json({error : errorString});
+    return;
+  }
+
+  try {
+    // Get game
+    const game = await Game.findById(req.params.game_id);
+
+    // Check whether the game is won
+    if (game.winner != null) {
+      res.json({
+        error: 'This game is finished. No more moves are allowed'
+      });
+      return;
+    }
+
+    // Check whether there is already done a move on the specified tile
+    if (game.boardState[yPos][xPos] != "-") {
+      const errorString = "There is already a cross or circle at tile x:" + xPos + ", y:" + yPos;
+      res.json({
+        error: errorString
+      });
+      return;
+    }
+
+    // Check if the right player is moving
+    if (game.nextToMove != player) {
+      const errorString = "It is not " + player + "'s turn.";
+      res.json({
+        error: errorString
+      });
+      return;
+    }
+
+    // Do the move
+    game.boardState[yPos][xPos] = player;
+    game.markModified('boardState'); // Needed to make sure mongoose knows the array is updated
+    game.nextToMove = nextPlayer;
+    const historyMove = [yPos, xPos, player];
+    game.history.push(historyMove);
+    const currentTime = Date();
+    game.lastMoveMade = currentTime;
+    await game.save();
+
+    // Send the game data back to the player
+    res.json(game);
+  }
+  catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
 export default router;
