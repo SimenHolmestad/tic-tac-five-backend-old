f992befe3023a69b841f8ff6b137db70b8e7553a
Simen Holmestad <simehol@stud.ntnu.no>
2020-01-06 18:23:28 +0100

    Add cors and allow all cors requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import gameRouter from "./routes/gameRoutes.js";
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+
+// Allow all cors requests
+app.use(cors());
 
 // Configure app to use bodyParser()
 // This will let us get the data from a POST
